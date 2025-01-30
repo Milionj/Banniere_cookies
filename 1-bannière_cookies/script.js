69fcec17aaae77196a8ac5b626abe804d6456338
Milionj <151533683+Milionj@users.noreply.github.com>
2025-01-30 16:45:16 +0100

    Add files via upload

---
+++ 1-bannière_cookies/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const btnSuccess = document.querySelector('.btn-success');
+    console.log(btnSuccess);
+
+    const cookies = document.querySelector('.cookies');
+    console.log(cookies);
+
+    btnSuccess.addEventListener('click', function() {
+        console.log('bouton cliqué !');
+        cookies.style.opacity = "0";
+});
+});
+
+
